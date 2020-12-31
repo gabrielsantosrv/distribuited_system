c1a8940242c9016badc766bff20e966eecceeaee
Gabriel <gabrielsantosrv@outlook.com>
2020-12-31 15:06:05 -0300

    updating public ip

--- lamport_server.py
+++ lamport_server.py
@@ -5,27 +5,25 @@ import time
 
 from lamport_utils import *
 
+# we use counter as a global variable to share among clients
+global counter
+counter = 0
+
 def handle_client(conn, client_ip):
     stop_loop = False
-
+    global counter
     with conn:
         while not stop_loop:
-            with open('counter.serialize') as file:
-                counter = int(file.readline())
-
             counter, keep_loop = receive_message(client_ip, conn, counter)
             if not keep_loop:
                 break
             msg = "[{}], the Server received your message".format(client_ip)
             counter = send_message(client_ip, conn, counter, msg)
-
-            with open('counter.serialize', 'w') as file:
-                file.write(str(counter))
-
             time.sleep(1)
         conn.close()
 
 def main():
+    global counter
     HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
     PORT = 9000  # Port to listen on (non-privileged ports are > 1023)
 
@@ -34,21 +32,11 @@ def main():
         sock.listen()
         print('Server started!')
         print('Waiting for clients...')
-        counter = 0
-        with open('counter.serialize', 'w') as file:
-            file.write(str(counter))
-
         while True:
-            with open('counter.serialize') as file:
-                counter = int(file.readline())
-
             conn, addr = sock.accept()
             lamport_timestamp = '(LAMPORT_TIMESTAMP={})'.format(counter)
             print('New client {} at {}'.format(addr, lamport_timestamp))
             counter += 1
-
-            with open('counter.serialize', 'w') as file:
-                file.write(str(counter))
             thread.start_new_thread(handle_client, (conn, addr))
         sock.close()
 
